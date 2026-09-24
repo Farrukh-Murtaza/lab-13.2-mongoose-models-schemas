e2d71be98d9ee88bda4abc2befe00bc87a291fb7
Farrukh-Murtaza <farrukhmurtaza1@gmail.com>
2026-09-24 13:53:27 -0400

    feat: initial commit. Project Setup.

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan")
+const app = express();
+const PORT = process.env.PORT || 3200;
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at localhost:${PORT}`);
+});
+
+
+
